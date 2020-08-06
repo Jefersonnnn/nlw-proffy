f930eebedc8c1d47b3b016577bf40b5b6fc47dfb
Jeferson Machado <jeferson_m94@hotmail.com>
2020-08-06 12:12:40 -0300

    database, route and migrations

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-
-// Corpo (Request Body): Dados para criação ou atualização de um registro
-// Route Params: Identificar qual recurso eu quero atualizar ou deletar (/:id)
-// Query Params: Paginação, filtros, ordenação (request.query) (/users?)
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World'});
-});
+app.use(routes)
 
 // localhost:3333
 app.listen(3333);
